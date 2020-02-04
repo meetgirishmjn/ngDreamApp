921ea061dfe3a5fe709a79484c64f502f40164d2
GIRISH MAHAJAN <GMAHA@monsanto.com>
2020-02-05 01:03:44 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +11,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MainHeaderComponent } from './components/main-header/mainHeader.component';
 import { MainSideMenuComponent } from './components/main-side-menu/MainSideMenu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatBadgeModule } from '@angular/material/badge';
+
 @NgModule({
   declarations: [
         AppComponent,
@@ -20,8 +23,9 @@ import { MainSideMenuComponent } from './components/main-side-menu/MainSideMenu.
     BrowserModule,
     AppRoutingModule,
       BrowserAnimationsModule,
+      FlexLayoutModule,
       MatButtonModule,
-      MatToolbarModule,
+      MatToolbarModule, MatMenuModule, MatBadgeModule,
       MatIconModule, MatSidenavModule, MatListModule
   ],
   providers: [],
